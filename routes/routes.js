10ab5fcd18da572bbe3bea073fe13787ec9442f2
TBleess <todd.bleess@state.co.us>
2026-01-23 14:16:08 -0700

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,8 @@
 //"use strict";
 
-//var pg = require('pg');
+var pg = require('pg');
      //import { Connector } from '@google-cloud/cloud-sql-connector';
-     import pg from 'pg';
+     //import pg from 'pg';
 
 var appRouter = function(app) {
 
@@ -194,7 +194,7 @@ var appRouter = function(app) {
 
             //var conString = "postgres://dola-gis-server:us-central1:free-trial-first-project:codemog:demography@34.55.5.64:5432/dola";  //this is a read only account, have fun!
           // var client = new pg.Client(conString);
-             const { Client} = pg;
+             const Client = pg;
              client = new Client({
                   user: 'codemog',
                   password: 'demography',
